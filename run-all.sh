2ef42dbb344adb69ed22d3744a45ce7c48d2b8f7
adileone <dileone.ale@gmail.com>
2019-06-06 12:43:10 +0200

    docker init

--- run-all.sh
+++ run-all.sh
@@ -4,19 +4,22 @@
 
 if echo dump | nc localhost 2181 | grep -q brokers ; then 
 
-	echo Running AVVISTAMENTI [Eureka + Kafka + Ribbon + Feign + Hystrix + Zuul Gateway]
+	echo Running AVVISTAMENTI [Eureka + Kafka + Ribbon + Feign + Hystrix + Zuul Gateway] &
 
 	echo Starting Eureka Server  
 	java -Xms64m -Xmx128m -jar eureka-server/build/libs/eureka-server-0.0.1-SNAPSHOT.jar &
-
-	echo Starting avvistamenti Services [servizioA:alice*1 + servizioB:bruno*1 + servizioC:carlo*1]
-	java -Xms64m -Xmx128m -jar -Dspring.profiles.active=alice servizioA/build/libs/servizioA-0.0.1-SNAPSHOT.jar &
-	java -Xms64m -Xmx128m -jar -Dspring.profiles.active=bruno servizioB/build/libs/servizioB-0.0.1-SNAPSHOT.jar &
-	java -Xms64m -Xmx128m -jar -Dspring.profiles.active=carlo servizioC/build/libs/servizioC-0.0.1-SNAPSHOT.jar &
-
-	echo Starting Zuul Gateway
-
-	java -Xms64m -Xmx128m -jar zuul-gateway/build/libs/zuul-gateway-0.0.1-SNAPSHOT.jar
+	sleep 2 &
+	echo Starting avvistamenti: servizioA:alice*1 &
+	java -Xms64m -Xmx128m -jar -Dspring.profiles.active=alice -Deureka.client.serviceUrl.defaultZone=http://localhost:8761/eureka/ -Dspring.kafka.bootstrap-servers=localhost:9092 servizioA/build/libs/servizioA-0.0.1-SNAPSHOT.jar &
+	sleep 2 &
+	echo Starting avvistamenti: servizioB:bruno*1 &
+	java -Xms64m -Xmx128m -jar -Dspring.profiles.active=bruno -Deureka.client.serviceUrl.defaultZone=http://localhost:8761/eureka/ -Dspring.kafka.bootstrap-servers=localhost:9092 servizioB/build/libs/servizioB-0.0.1-SNAPSHOT.jar &
+	sleep 2 &
+	echo Starting avvistamenti: servizioC:carlo*1 &
+	java -Xms64m -Xmx128m -jar -Dspring.profiles.active=carlo -Deureka.client.serviceUrl.defaultZone=http://localhost:8761/eureka/ servizioC/build/libs/servizioC-0.0.1-SNAPSHOT.jar &
+	sleep 2 &
+	echo Starting Zuul Gateway &
+	java -Xms64m -Xmx128m -jar -Deureka.client.serviceUrl.defaultZone=http://localhost:8761/eureka/ zuul-gateway/build/libs/zuul-gateway-0.0.1-SNAPSHOT.jar
 
 else 
 		echo please start Kafka first ... run start-kafka.sh as root
---
+++ set-docker-host-ip.sh
@@ -0,0 +1,8 @@
+MY_IP_ADDR=$(ifconfig  | grep 'inet' | grep -v 'inet6' | grep -v '127.0.0.1' | cut -d: -f2 | awk '{ print $2}' | grep '172.22.0.')
+
+if [ -z "$DOCKER_HOST_IP" ] ; then
+    echo "please source this file" 
+    export DOCKER_HOST_IP=${MY_IP_ADDR}
+fi
+
+echo DOCKER_HOST_IP is ${DOCKER_HOST_IP}
